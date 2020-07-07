7b39681bef0278b1303dd55ab70b7b6a96e39038
MrPandaLine <romasuvalov19@gmail.com>
2020-07-07 15:24:17 +0300

    Completed API

--- soup/settings.py
+++ soup/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '(m06+ltaqrr(w9#x%nsnh*vacwv-8u+g4dsg$9j=rn@lznb3x7'
+GITHUB_WEBHOOK_KEY = '7dKRmkshtBPfCJx6ly7JJzkI24Tew5XPlAoVgFOwm8XyGzuoNz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
